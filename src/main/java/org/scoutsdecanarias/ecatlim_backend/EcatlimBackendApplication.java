e135696ab38fb71712ca8a031dfabb9287188182
Pablo Nicolás <pablo.santana116@alu.ulpgc.es>
2025-03-02 22:52:02 +0000

    First Commit
    Configured Spring Boot project

---
+++ src/main/java/org/scoutsdecanarias/ecatlim_backend/EcatlimBackendApplication.java
@@ -0,0 +1,13 @@
+package org.scoutsdecanarias.ecatlim_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcatlimBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcatlimBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/scoutsdecanarias/ecatlim_backend/EcatlimBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.scoutsdecanarias.ecatlim_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcatlimBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
