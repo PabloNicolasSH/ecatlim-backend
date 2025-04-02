1cbf1af7dbe4b8967882bce413ed674e31d10661
Pablo Nicolás <pablo.santana116@alu.ulpgc.es>
2025-04-02 23:54:28 +0100

    ECL-6 Updated User Controller, Service and Repository

--- src/main/java/org/scoutsdecanarias/ecatlim_backend/controller/UserController.java
+++ src/main/java/org/scoutsdecanarias/ecatlim_backend/controller/UserController.java
@@ -6,8 +6,11 @@ import org.scoutsdecanarias.ecatlim_backend.entity.User;
 import org.scoutsdecanarias.ecatlim_backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,9 +35,14 @@ public class UserController {
         return UserProfileDto.fromEntity(userService.getUserByEmail(principal.getName()));
     }
 
-    @GetMapping("/admin/all")
-    public List<UserProfileDto> getUsers() {
-        return UserProfileDto.fromCollection(userService.getUsers());
+    @GetMapping("/admin/allActives")
+    public List<UserProfileDto> getActiveUsers() {
+        return UserProfileDto.fromCollection(userService.getActiveUsers());
+    }
+
+    @GetMapping("/admin/allInactives")
+    public List<UserProfileDto> getInactiveUsers() {
+        return UserProfileDto.fromCollection(userService.getInactiveUsers());
     }
 
     @PostMapping("/admin/add")
@@ -42,4 +50,23 @@ public class UserController {
         log.info("Adding user: {}", user);
         return UserProfileDto.fromEntity(userService.addUser(user));
     }
+
+    @PutMapping("/admin/edit/{id}")
+    public UserProfileDto editUser(@PathVariable Integer id, @RequestBody User user) {
+        log.info("Updating user: {}", user);
+        return UserProfileDto.fromEntity(userService.updateUser(id, user));
+    }
+
+    @PutMapping("/admin/activate/{id}")
+    public UserProfileDto activateUser(@PathVariable Integer id) {
+        log.info("Activating user: {}", id);
+        return UserProfileDto.fromEntity(userService.activateUser(id));
+    }
+
+    @DeleteMapping("/admin/deactivate/{id}")
+    public UserProfileDto deactivateUser(@PathVariable Integer id) {
+        log.info("Deactivating user: {}", id);
+        return UserProfileDto.fromEntity(userService.deactivateUser(id));
+    }
+
 }
--- src/main/java/org/scoutsdecanarias/ecatlim_backend/service/UserService.java
+++ src/main/java/org/scoutsdecanarias/ecatlim_backend/service/UserService.java
@@ -1,5 +1,8 @@
 package org.scoutsdecanarias.ecatlim_backend.service;
 
+import org.passay.CharacterRule;
+import org.passay.EnglishCharacterData;
+import org.passay.PasswordGenerator;
 import org.scoutsdecanarias.ecatlim_backend.entity.User;
 import org.scoutsdecanarias.ecatlim_backend.enums.Role;
 import org.scoutsdecanarias.ecatlim_backend.repository.UserRepository;
@@ -25,8 +28,12 @@ public class UserService implements UserDetailsService {
         this.userRepository = userRepository;
     }
 
-    public List<User> getUsers() {
-        return userRepository.findAll();
+    public List<User> getActiveUsers() {
+        return userRepository.findAllByEnabled(true);
+    }
+
+    public List<User> getInactiveUsers() {
+        return userRepository.findAllByEnabled(false);
     }
 
     public List<User> getUsersByRole(Role role) {
@@ -48,16 +55,21 @@ public class UserService implements UserDetailsService {
     }
 
     public User addUser(User user) {
+        PasswordGenerator passwordGenerator = new PasswordGenerator();
+        user.setPassword(passwordGenerator
+                .generatePassword(12, new CharacterRule(EnglishCharacterData.Alphabetical, 7), new CharacterRule(EnglishCharacterData.Digit, 3)));
+        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
+            throw new RuntimeException();
+        }
         return userRepository.save(user);
     }
 
-    public User updateUser(User user) {
-        User updatedUser = userRepository.findByEmail(user.getEmail()).orElseThrow(() -> new UsernameNotFoundException(user.getEmail()));
+    public User updateUser(Integer id, User user) {
+        User updatedUser = userRepository.findById(id).orElseThrow(() -> new UsernameNotFoundException(id.toString()));
 
         updatedUser.setName(user.getName());
         updatedUser.setSurname(user.getSurname());
         updatedUser.setEmail(user.getEmail());
-        updatedUser.setPassword(user.getPassword());
         updatedUser.setNif(user.getNif());
         updatedUser.setRole(user.getRole());
         updatedUser.setCountry(user.getCountry());
@@ -82,5 +94,17 @@ public class UserService implements UserDetailsService {
         Set<GrantedAuthority> authorities = new HashSet<>();
         authorities.add(new SimpleGrantedAuthority(userRole.name()));
         return new ArrayList<>(authorities);
+    }
+
+    public User activateUser(Integer id) {
+        User userToActive = userRepository.findById(id).orElseThrow(() -> new UsernameNotFoundException(id.toString()));
+        userToActive.setEnabled(true);
+        return userRepository.save(userToActive);
+    }
+
+    public User deactivateUser(Integer id) {
+        User user = userRepository.findById(id).orElseThrow(() -> new UsernameNotFoundException(id.toString()));
+        user.setEnabled(false);
+        return userRepository.save(user);
     }
 }
