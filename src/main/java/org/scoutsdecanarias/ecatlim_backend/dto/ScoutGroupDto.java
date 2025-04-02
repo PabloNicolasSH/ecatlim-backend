21da9ea61a714fd311c7465145c0bf692d6c3241
Pablo Nicolás <pablo.santana116@alu.ulpgc.es>
2025-04-02 23:54:00 +0100

    ECL-6 Updated dto's

--- src/main/java/org/scoutsdecanarias/ecatlim_backend/dto/ScoutGroupDto.java
+++ src/main/java/org/scoutsdecanarias/ecatlim_backend/dto/ScoutGroupDto.java
@@ -2,13 +2,23 @@ package org.scoutsdecanarias.ecatlim_backend.dto;
 
 import org.scoutsdecanarias.ecatlim_backend.entity.ScoutGroup;
 
-public record ScoutGroupDto(String name, int provinceId, int groupNumber) {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public record ScoutGroupDto(Integer id, String name, int provinceId, int groupNumber) {
 
     public static ScoutGroupDto fromEntity(ScoutGroup scoutGroup) {
+        if (scoutGroup == null) return null;
         return new ScoutGroupDto(
+                scoutGroup.getId(),
                 scoutGroup.getName(),
                 scoutGroup.getProvinceId(),
                 scoutGroup.getGroupNumber()
         );
+    }
+
+    public static List<ScoutGroupDto> fromCollection(List<ScoutGroup> groups) {
+        return groups.stream().map(ScoutGroupDto::fromEntity).collect(Collectors.toList());
     }
 }
--- src/main/java/org/scoutsdecanarias/ecatlim_backend/dto/UserProfileDto.java
+++ src/main/java/org/scoutsdecanarias/ecatlim_backend/dto/UserProfileDto.java
@@ -5,15 +5,17 @@ import org.scoutsdecanarias.ecatlim_backend.entity.User;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public record UserProfileDto(String name, String surname, String email, String phone, Integer census, String nif, String address, String city, String country, ScoutGroupDto scoutGroup) {
+public record UserProfileDto(Integer id, String name, String surname, String email, String phone, Integer census, String role, String nif, String address, String city, String country, ScoutGroupDto scoutGroup) {
 
     public static UserProfileDto fromEntity(User user){
         return new UserProfileDto(
+                user.getId(),
                 user.getName(),
                 user.getSurname(),
                 user.getEmail(),
                 user.getPhone(),
                 user.getCensus(),
+                user.getRole().name(),
                 user.getNif(),
                 user.getAddress(),
                 user.getCity(),
