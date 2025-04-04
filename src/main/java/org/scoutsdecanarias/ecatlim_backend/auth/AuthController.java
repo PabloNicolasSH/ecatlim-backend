a1f55ce99246878587784cb2a7f86666d741faa6
Pablo Nicolás <pablo.santana116@alu.ulpgc.es>
2025-04-04 01:07:00 +0100

    ECL-6 Updated AuthController and AuthRequest
    Updated AuthController and AuthRequest to catch somo user fields on the frontend to save it in the local storage

--- src/main/java/org/scoutsdecanarias/ecatlim_backend/auth/AuthController.java
+++ src/main/java/org/scoutsdecanarias/ecatlim_backend/auth/AuthController.java
@@ -2,8 +2,10 @@ package org.scoutsdecanarias.ecatlim_backend.auth;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.scoutsdecanarias.ecatlim_backend.entity.User;
+import org.scoutsdecanarias.ecatlim_backend.enums.Role;
+import org.scoutsdecanarias.ecatlim_backend.repository.UserRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
@@ -21,26 +24,35 @@ public class AuthController {
     private final AuthenticationManager authenticationManager;
 
     private final JwtUtil jwtUtil;
+    private final UserRepository userRepository;
 
-    public AuthController(AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
+    public AuthController(AuthenticationManager authenticationManager, JwtUtil jwtUtil, UserRepository userRepository) {
         this.authenticationManager = authenticationManager;
         this.jwtUtil = jwtUtil;
+        this.userRepository = userRepository;
     }
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody AuthRequest request) {
+        log.info("Login request: {}", request);
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword())
         );
 
         String token = jwtUtil.generateToken((UserDetails) authentication.getPrincipal());
+        User user = userRepository.findByEmail(request.getUsername()).orElse(null);
 
-        return ResponseEntity.ok(new AuthResponse(token));
+        assert user != null;
+        return ResponseEntity.ok(new AuthResponse(token, user.getName(), user.getSurname(), user.getEmail(), user.getRole()));
     }
 
     @Data
     @AllArgsConstructor
     class AuthResponse {
         private String token;
+        private String name;
+        private String surname;
+        private String email;
+        private Role role;
     }
 }
