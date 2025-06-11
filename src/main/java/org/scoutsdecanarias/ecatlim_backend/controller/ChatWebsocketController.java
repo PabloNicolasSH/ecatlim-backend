ad9ddf1b07e2c7b841744ea99d684dd3c8065cc9
Pablo Nicolás <pablo.santana116@alu.ulpgc.es>
2025-06-11 11:17:24 +0100

    ECL-12 Updated websocket for multithreads

--- src/main/java/org/scoutsdecanarias/ecatlim_backend/controller/ChatWebsocketController.java
+++ src/main/java/org/scoutsdecanarias/ecatlim_backend/controller/ChatWebsocketController.java
@@ -1,31 +1,45 @@
 package org.scoutsdecanarias.ecatlim_backend.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.scoutsdecanarias.ecatlim_backend.dto.ChatMessageDto;
 import org.scoutsdecanarias.ecatlim_backend.entity.ChatMessage;
 import org.scoutsdecanarias.ecatlim_backend.service.ChatService;
 import org.scoutsdecanarias.ecatlim_backend.service.UserService;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 import java.util.Map;
 
+@Slf4j
 @Controller
 public class ChatWebsocketController {
 
     private final UserService userService;
     private final ChatService chatService;
+    private final SimpMessagingTemplate template;
 
-    public ChatWebsocketController(UserService userService, ChatService chatService) {
+    public ChatWebsocketController(UserService userService, ChatService chatService, SimpMessagingTemplate template) {
         this.userService = userService;
         this.chatService = chatService;
+        this.template = template;
     }
 
-    @MessageMapping("/chat.send")
-    @SendTo("/topic/messages")
-    public void sendMessage(ChatMessage message, @Header("simpSessionAttributes") Map<String, Object> attributes) {
+    @MessageMapping("/chat/{chatId}/send")
+    @SendTo("/topic/chat/{chatId}")
+    public ChatMessageDto sendMessage(@DestinationVariable Integer chatId, ChatMessageDto messageDto, @Header("simpSessionAttributes") Map<String, Object> attributes) {
+        log.info("METHOD sendMessage() - Called by {}", attributes.get("username"));
+
+        ChatMessage message = new ChatMessage();
+
         String username = (String) attributes.get("username");
         message.setFrom(userService.getUserByEmail(username));
-        chatService.saveAndSend(message);
+        message.setMessage(messageDto.message());
+        message.setChat(chatService.getChatById(messageDto.to()));
+
+        return ChatMessageDto.fromEntity(chatService.saveChatMessage(message));
     }
 }
