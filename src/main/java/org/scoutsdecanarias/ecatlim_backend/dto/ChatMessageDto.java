045cb7364c5fb9bd99500886407ac447c472e76f
Pablo Nicolás <pablo.santana116@alu.ulpgc.es>
2025-11-14 00:42:20 +0000

    ECL-12 Updated ChatMessageDto and added SimpleUserDto

--- src/main/java/org/scoutsdecanarias/ecatlim_backend/dto/ChatMessageDto.java
+++ src/main/java/org/scoutsdecanarias/ecatlim_backend/dto/ChatMessageDto.java
@@ -5,21 +5,27 @@ import org.scoutsdecanarias.ecatlim_backend.entity.ChatMessage;
 import java.util.List;
 
 public record ChatMessageDto(
-        String from,
-        Integer to,
+        Integer id,
+        SimpleUserDto from,
+        Integer chatId,
         String message,
-        String timestamp
+        String timestamp,
+        boolean read
 ) {
     public static ChatMessageDto fromEntity(ChatMessage chatMessage) {
         return new ChatMessageDto(
-                chatMessage.getFrom().getEmail(),
+                chatMessage.getId(),
+                SimpleUserDto.fromEntity(chatMessage.getFrom()),
                 chatMessage.getChat().getId(),
                 chatMessage.getMessage(),
-                chatMessage.getTimestamp().toString()
+                chatMessage.getTimestamp().toString(),
+                chatMessage.isRead()
         );
     }
 
     public static List<ChatMessageDto> fromCollection(List<ChatMessage> chatHistory) {
-        return chatHistory.stream().map(ChatMessageDto::fromEntity).toList();
+        return chatHistory.stream()
+                .map(ChatMessageDto::fromEntity)
+                .toList();
     }
 }
---
+++ src/main/java/org/scoutsdecanarias/ecatlim_backend/dto/SimpleUserDto.java
@@ -0,0 +1,17 @@
+package org.scoutsdecanarias.ecatlim_backend.dto;
+
+import org.scoutsdecanarias.ecatlim_backend.entity.User;
+
+public record SimpleUserDto(
+        Integer id,
+        String name,
+        String email
+) {
+    public static SimpleUserDto fromEntity(User user) {
+        return new SimpleUserDto(
+                user.getId(),
+                user.getName(),
+                user.getEmail()
+        );
+    }
+}
