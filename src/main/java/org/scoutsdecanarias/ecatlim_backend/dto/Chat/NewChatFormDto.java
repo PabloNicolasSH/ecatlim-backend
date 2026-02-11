1c93b9f91d04e5ab23b20aeacc4a297b49927475
Pablo Nicolás <pablo.santana116@alu.ulpgc.es>
2026-02-11 20:46:22 +0000

    ECL-12 Deleted NotBlank decoration in NewChatFormDto

--- src/main/java/org/scoutsdecanarias/ecatlim_backend/dto/Chat/NewChatFormDto.java
+++ src/main/java/org/scoutsdecanarias/ecatlim_backend/dto/Chat/NewChatFormDto.java
@@ -1,12 +1,11 @@
 package org.scoutsdecanarias.ecatlim_backend.dto.Chat;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 
 import java.util.List;
 
 public record NewChatFormDto(
-        @NotBlank String name,
+        String name,
         String description,
         @NotEmpty List<Integer> chatMembers
 ) {
