f8aec81a1c3af34bb60afe560ba599d1768f7666
Pablo Nicolás <pablo.santana116@alu.ulpgc.es>
2026-06-19 14:50:00 +0100

    ECL-11 Moved elements

--- src/main/java/org/scoutsdecanarias/ecatlim_backend/features/lesson_block/BlockDetailDto.java
+++ src/main/java/org/scoutsdecanarias/ecatlim_backend/features/lesson_block/BlockDetailDto.java
@@ -1,6 +1,6 @@
 package org.scoutsdecanarias.ecatlim_backend.features.lesson_block;
 
-import org.scoutsdecanarias.ecatlim_backend.features.activity.ActivityDetailDto;
+import org.scoutsdecanarias.ecatlim_backend.features.activity.dto.ActivityDetailDto;
 
 import java.util.Date;
 import java.util.List;
