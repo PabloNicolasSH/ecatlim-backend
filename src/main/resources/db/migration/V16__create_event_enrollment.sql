1fd679148c554ccbc73ad781c2884e87f98c1956
Pablo Nicolás <pablo.santana116@alu.ulpgc.es>
2026-06-19 12:43:00 +0100

    ECL-22 Added migration to create event enrollment

--- src/main/resources/db/migration/V16__create_event_enrollment.sql
+++ src/main/resources/db/migration/V16__create_event_enrollment.sql
@@ -1,21 +1,24 @@
+DROP TABLE IF EXISTS event_enrollments;
+
 CREATE TABLE event_enrollment
 (
     id              INT AUTO_INCREMENT NOT NULL,
     event_id        INT                NOT NULL,
     user_id         INT                NOT NULL,
     lesson_block_id INT                NOT NULL,
-    payment_state   VARCHAR(255),
-    has_attended    BIT(1)             NOT NULL,
+    payment_status  VARCHAR(50)        NOT NULL,
+    has_attended    BOOLEAN            NOT NULL DEFAULT FALSE,
     CONSTRAINT pk_eventenrollment PRIMARY KEY (id)
 );
 
 ALTER TABLE event_enrollment
-    ADD CONSTRAINT FK_EVENTENROLLMENT_ON_EVENT FOREIGN KEY (event_id) REFERENCES event (id);
+    ADD CONSTRAINT FK_EVENTENROLLMENT_ON_EVENT
+        FOREIGN KEY (event_id) REFERENCES event (id) ON DELETE CASCADE;
 
 ALTER TABLE event_enrollment
-    ADD CONSTRAINT FK_EVENTENROLLMENT_ON_LESSON_BLOCK FOREIGN KEY (lesson_block_id) REFERENCES lesson_block (id);
+    ADD CONSTRAINT FK_EVENTENROLLMENT_ON_USER
+        FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE;
 
 ALTER TABLE event_enrollment
-    ADD CONSTRAINT FK_EVENTENROLLMENT_ON_USER FOREIGN KEY (user_id) REFERENCES user (id);
-
-DROP TABLE event_enrollments;
+    ADD CONSTRAINT FK_EVENTENROLLMENT_ON_LESSON_BLOCK
+        FOREIGN KEY (lesson_block_id) REFERENCES lesson_block (id) ON DELETE CASCADE;
